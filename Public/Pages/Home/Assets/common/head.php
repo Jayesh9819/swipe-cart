487c1eeaefb297721f2d44bdc19e434dcb8b6c23
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-03 21:55:50 +0530

    Update head.php

--- Public/Pages/Home/Assets/common/head.php
+++ Public/Pages/Home/Assets/common/head.php
@@ -25,7 +25,8 @@
 
     <!-- bootstrap css -->
     <link id="rtl-link" rel="stylesheet" type="text/css" href="../Other/Home_assets/assets/css/vendors/bootstrap.css">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- wow css -->
     <link rel="stylesheet" href="../Other/Home_assets/assets/css/animate.min.css" />
 
