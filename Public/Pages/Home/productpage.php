b16add5feb85b628d4a9767e55beda121e749c0a
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-03 19:10:01 +0530

    Update productpage.php

--- Public/Pages/Home/productpage.php
+++ Public/Pages/Home/productpage.php
@@ -106,7 +106,7 @@
                                                     <div class="bottom-slider-image left-slider no-arrow slick-top" style="height: 100px; width: 50px;">
                                                         <div>
                                                             <div class="sidebar-image">
-                                                                <img src="<?php echo $row['main_img']; ?>" style="width: 100%; height: 100%; object-fit: contain; display: block;" class="img-fluid blur-up lazyload" alt="">
+                                                                <img src="<?php echo $row['main_img']; ?>" style="width: 100%; height: 100%; object-fit: contain;" class="img-fluid blur-up lazyload" alt="">
                                                             </div>
                                                         </div>
                                                         <?php
@@ -117,7 +117,7 @@
                                                         <?php foreach ($sidebar_images as $index => $sidebar_image) : ?>
                                                             <div>
                                                                 <div class="sidebar-image">
-                                                                    <img src="<?php echo '../' . $sidebar_image; ?>" style="width: 100%; height: 100%; object-fit: contain; display: block;" class="img-fluid blur-up lazyload" alt="">
+                                                                    <img src="<?php echo '../' . $sidebar_image; ?>" style="width: 100%; height: 100%; object-fit: contain;" class="img-fluid blur-up lazyload" alt="">
                                                                 </div>
                                                             </div>
                                                         <?php endforeach; ?>
