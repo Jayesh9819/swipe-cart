25f92cdd430a5d2fa07d097e6d94e0f7f053cdd3
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-03 19:17:55 +0530

    Update productpage.php

--- Public/Pages/Home/productpage.php
+++ Public/Pages/Home/productpage.php
@@ -117,7 +117,7 @@
                                                         <?php foreach ($sidebar_images as $index => $sidebar_image) : ?>
                                                             <div>
                                                                 <div class="sidebar-image">
-                                                                    <img src="<?php echo '../' . $sidebar_image; ?>" style="width: 100%; height: 100%; object-fit: contain;" class="img-fluid blur-up lazyload" alt="">
+                                                                    <img src="<?php echo '../' . $sidebar_image; ?>" style="width: 100px; height: 100px; object-fit: contain;" class="img-fluid blur-up lazyload" alt="">
                                                                 </div>
                                                             </div>
                                                         <?php endforeach; ?>
