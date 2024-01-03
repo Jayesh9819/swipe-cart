7120a993f4a45cf301417acce98cb224966e723f
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-03 21:35:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
    include './App/Logic/db_connect.php';
    $sql = "SELECT * FROM settings";
    $result = $conn->query($sql);
+  global $site;
    $site=$result->fetch_assoc();
    print_r($site);
 
