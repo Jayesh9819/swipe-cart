2ff4b9bd372cb36d481ce6a7df115370bb62ecc9
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-04 17:24:47 +0530

    s

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
+session_start();
 require "./Router/router.php";
 
-   include './App/Logic/db_connect.php';
-   $sql = "SELECT * FROM settings";
-   $result = $conn->query($sql);
-  global $site;
-   $site=$result->fetch_assoc();
-   print_r($site);
 
 ?>
