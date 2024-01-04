df2ad441ed0faf9fc604e9e029b27fe1a07d786f
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-04 17:39:12 +0530

    Update signup.php

--- Public/Pages/Login/Assets/SignUp/signup.php
+++ Public/Pages/Login/Assets/SignUp/signup.php
@@ -1,176 +1,183 @@
    <!-- Breadcrumb Section Start -->
-   <?php 
+   <?php
 
-include './App/Logic/db_connect.php';
-$sql = "SELECT * FROM settings";
-$result = $conn->query($sql);
-global $site;
-$site=$result->fetch_assoc();
+    include './App/Logic/db_connect.php';
+    $sql = "SELECT * FROM settings";
+    $result = $conn->query($sql);
+    global $site;
+    $site = $result->fetch_assoc();
 
 
-   ?>
+    ?>
+   <?php
+    $act = isset($_SESSION['act']) ? $_SESSION['act'] : '';
+    switch ($act) {
+        case 'pass':
+            echo $pass;
+            break;
+        case 'duplicate':
+            echo $duplicate;
+    }
+    ?>
+
 
    <section class="breadscrumb-section pt-0">
-        <div class="container-fluid-lg">
-            <div class="row">
-                <div class="col-12">
-                    <div class="breadscrumb-contain">
-                        <h2>Sign In</h2>
-                        <nav>
-                            <ol class="breadcrumb mb-0">
-                                <li class="breadcrumb-item">
-                                    <a href="index.html">
-                                        <i class="fa-solid fa-house"></i>
-                                    </a>
-                                </li>
-                                <li class="breadcrumb-item active">Sign In</li>
-                            </ol>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Breadcrumb Section End -->
-    <script>
-        $(document).ready(function() {
-            $('#password, #confirm-password').on('input', function() {
-                var password = $('#password').val();
-                var confirmPassword = $('#confirm-password').val();
+       <div class="container-fluid-lg">
+           <div class="row">
+               <div class="col-12">
+                   <div class="breadscrumb-contain">
+                       <h2>Sign In</h2>
+                       <nav>
+                           <ol class="breadcrumb mb-0">
+                               <li class="breadcrumb-item">
+                                   <a href="index.html">
+                                       <i class="fa-solid fa-house"></i>
+                                   </a>
+                               </li>
+                               <li class="breadcrumb-item active">Sign In</li>
+                           </ol>
+                       </nav>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!-- Breadcrumb Section End -->
+   <script>
+       $(document).ready(function() {
+           $('#password, #confirm-password').on('input', function() {
+               var password = $('#password').val();
+               var confirmPassword = $('#confirm-password').val();
 
-                if (password !== confirmPassword) {
-                    $('#password-error').html('Passwords do not match');
-                } else {
-                    $('#password-error').html('');
-                }
-            });
-        });
-    </script>
+               if (password !== confirmPassword) {
+                   $('#password-error').html('Passwords do not match');
+               } else {
+                   $('#password-error').html('');
+               }
+           });
+       });
+   </script>
 
-    <!-- log in section start -->
-    <section class="log-in-section section-b-space">
-        <div class="container-fluid-lg w-100">
-            <div class="row">
-                <div class="col-xxl-6 col-xl-5 col-lg-6 d-lg-block d-none ms-auto">
-                    <div class="image-contain">
-                        <img src="../Other/Home_assets/assets/images/inner-page/sign-up.png" class="img-fluid" alt="">
-                    </div>
-                </div>
+   <!-- log in section start -->
+   <section class="log-in-section section-b-space">
+       <div class="container-fluid-lg w-100">
+           <div class="row">
+               <div class="col-xxl-6 col-xl-5 col-lg-6 d-lg-block d-none ms-auto">
+                   <div class="image-contain">
+                       <img src="../Other/Home_assets/assets/images/inner-page/sign-up.png" class="img-fluid" alt="">
+                   </div>
+               </div>
 
-                <div class="col-xxl-4 col-xl-5 col-lg-6 col-sm-8 mx-auto">
-                    <div class="log-in-box">
-                        <div class="log-in-title">
-                            <h3>Welcome To <?php echo $site['Site-name']; ?></h3>
-                            <h4>Create New Account</h4>
-                        </div>
+               <div class="col-xxl-4 col-xl-5 col-lg-6 col-sm-8 mx-auto">
+                   <div class="log-in-box">
+                       <div class="log-in-title">
+                           <h3>Welcome To <?php echo $site['Site-name']; ?></h3>
+                           <h4>Create New Account</h4>
+                       </div>
 
-                        <div class="input-box">
-                            <form class="row g-4" action="../App/Logic/Login.php?action=create_user " method="post" >
-                                <div class="col-12">
-                                    <div class="form-floating theme-form-floating">
-                                        <input type="text" class="form-control"  name="name" id="fullname" placeholder="Full Name" required>
-                                        <label for="fullname">Full Name</label>
-                                    </div>
+                       <div class="input-box">
+                           <form class="row g-4" action="../App/Logic/Login.php?action=create_user " method="post">
+                               <div class="col-12">
+                                   <div class="form-floating theme-form-floating">
+                                       <input type="text" class="form-control" name="name" id="fullname" placeholder="Full Name" required>
+                                       <label for="fullname">Full Name</label>
+                                   </div>
+                               </div>
+                               <div class="col-12">
+                                   <div class="form-floating theme-form-floating">
+                                       <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" required>
+                                       <label for="email">Email Address</label>
+                                   </div>
+                               </div>
+                               <?php
+                                $duplicate = '<div class="alert alert-danger d-flex align-items-center" role="alert">
+                                <div>
+                                Email or phone already Registerd
                                 </div>
-                                <div class="col-12">
-                                    <div class="form-floating theme-form-floating">
-                                        <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" required>
-                                        <label for="email">Email Address</label>
-                                    </div>
+                              </div>
+                              ';
+                                ?>
+                               <div class="col-12">
+                                   <div class="form-floating theme-form-floating">
+                                       <input type="number" class="form-control" name="mobile" id="email" placeholder="Mobile Number" required>
+                                       <label for="email">Mobile Number</label>
+                                   </div>
+                               </div>
+                               <?php
+                                $pass = '<div class="alert alert-danger d-flex align-items-center" role="alert">
+                                <div>
+                                  Password Does Not match
                                 </div>
-                                <div class="col-12">
-                                    <div class="form-floating theme-form-floating">
-                                        <input type="number" class="form-control" name="mobile" id="email" placeholder="Mobile Number" required>
-                                        <label for="email">Mobile Number</label>
-                                    </div>
-                                </div>
-                                <?php 
-                                $act = isset($_SESSION['act'])? $_SESSION['act']:'';
-                                switch ($act) {
-                                    case 'pass':
-                                        echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
-                                        <div>
-                                          Password Does Not match
-                                        </div>
-                                      </div>
-                                      ';
-                                        break;
-                                
-                                    }
+                              </div>
+                              ';
                                 ?>
 
 
-                                <div class="col-12">
-                                    <div class="form-floating theme-form-floating">
-                                        <input type="password" class="form-control" name="pass" id="password"
-                                            placeholder="Password" required>
-                                        <label for="password">Password</label>
-                                    </div>
-                                </div>
+                               <div class="col-12">
+                                   <div class="form-floating theme-form-floating">
+                                       <input type="password" class="form-control" name="pass" id="password" placeholder="Password" required>
+                                       <label for="password">Password</label>
+                                   </div>
+                               </div>
 
-                                <div class="col-12">
-                                    <div class="form-floating theme-form-floating">
-                                        <input type="password" class="form-control" name="con-pass" id="password"
-                                            placeholder="Password" required>
-                                        <label for="password">Confirm Password</label>
-                                    </div>
-                                </div>
-    <!-- <div id="password-error" class="alert alert-danger d-flex align-items-center" role="alert"></div> -->
+                               <div class="col-12">
+                                   <div class="form-floating theme-form-floating">
+                                       <input type="password" class="form-control" name="con-pass" id="password" placeholder="Password" required>
+                                       <label for="password">Confirm Password</label>
+                                   </div>
+                               </div>
+                               <!-- <div id="password-error" class="alert alert-danger d-flex align-items-center" role="alert"></div> -->
 
 
-                                <div class="col-12">
-                                    <div class="forgot-box">
-                                        <div class="form-check ps-0 m-0 remember-box">
-                                            <input class="checkbox_animated check-box" type="checkbox"
-                                                id="flexCheckDefault">
-                                            <label class="form-check-label" for="flexCheckDefault">I agree with
-                                                <span>Terms</span> and <span>Privacy</span></label>
-                                        </div>
-                                    </div>
-                                </div>
+                               <div class="col-12">
+                                   <div class="forgot-box">
+                                       <div class="form-check ps-0 m-0 remember-box">
+                                           <input class="checkbox_animated check-box" type="checkbox" id="flexCheckDefault">
+                                           <label class="form-check-label" for="flexCheckDefault">I agree with
+                                               <span>Terms</span> and <span>Privacy</span></label>
+                                       </div>
+                                   </div>
+                               </div>
 
-                                <div class="col-12">
-                                    <button class="btn btn-animation w-100" type="submit">Sign Up</button>
-                                </div>
-                            </form>
-                        </div>
+                               <div class="col-12">
+                                   <button class="btn btn-animation w-100" type="submit">Sign Up</button>
+                               </div>
+                           </form>
+                       </div>
 
-                        <div class="other-log-in">
-                            <h6>or</h6>
-                        </div>
+                       <div class="other-log-in">
+                           <h6>or</h6>
+                       </div>
 
-                        <div class="log-in-button">
-                            <ul>
-                                <li>
-                                    <a href="https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&amp;flowEntry=ServiceLogin"
-                                        class="btn google-button w-100">
-                                        <img src="../Other/Home_assets/assets/images/inner-page/google.png" class="blur-up lazyload"
-                                            alt="">
-                                        Sign up with Google
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://www.facebook.com/" class="btn google-button w-100">
-                                        <img src="../Other/Home_assets/assets/images/inner-page/facebook.png" class="blur-up lazyload"
-                                            alt=""> Sign up with Facebook
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                       <div class="log-in-button">
+                           <ul>
+                               <li>
+                                   <a href="https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&amp;flowEntry=ServiceLogin" class="btn google-button w-100">
+                                       <img src="../Other/Home_assets/assets/images/inner-page/google.png" class="blur-up lazyload" alt="">
+                                       Sign up with Google
+                                   </a>
+                               </li>
+                               <li>
+                                   <a href="https://www.facebook.com/" class="btn google-button w-100">
+                                       <img src="../Other/Home_assets/assets/images/inner-page/facebook.png" class="blur-up lazyload" alt=""> Sign up with Facebook
+                                   </a>
+                               </li>
+                           </ul>
+                       </div>
 
-                        <div class="other-log-in">
-                            <h6></h6>
-                        </div>
+                       <div class="other-log-in">
+                           <h6></h6>
+                       </div>
 
-                        <div class="sign-up-box">
-                            <h4>Already have an account?</h4>
-                            <a href="./login">Log In</a>
-                        </div>
-                    </div>
-                </div>
+                       <div class="sign-up-box">
+                           <h4>Already have an account?</h4>
+                           <a href="./login">Log In</a>
+                       </div>
+                   </div>
+               </div>
 
-                <div class="col-xxl-7 col-xl-6 col-lg-6"></div>
-            </div>
-        </div>
-    </section>
-    <!-- log in section end -->
+               <div class="col-xxl-7 col-xl-6 col-lg-6"></div>
+           </div>
+       </div>
+   </section>
+   <!-- log in section end -->
