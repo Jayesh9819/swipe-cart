62be8e52912e65669784bd8ff26d9d1df0f858c7
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-10 20:48:57 +0530

    Update productpage.php

--- Public/Pages/Home/productpage.php
+++ Public/Pages/Home/productpage.php
@@ -168,7 +168,7 @@
                                                 $uid = $_SESSION['id'];
                                                 // echo '<a href="../App/Logic/order.php?action=addtocart&uid=' . $uid . '&pid=' . $id . '&quantity=" class="btn btn-md bg-dark cart-button text-white w-100" id="addToCartBtn">Add To Cart</a>';
 
-                                                echo '<a href="javascript:void(0);" class="btn btn-md bg-dark cart-button text-white w-100" onclick="addToCart(' . $uid . ',' . $id . ',1)" id="addToCartBtn">Add To Cart</a>';
+                                                echo '<a href="javascript:void(0);" class="btn btn-md bg-dark cart-button text-white w-100" onclick="addToCart(' . $uid . ',' . $id . ',0)" id="addToCartBtn">Add To Cart</a>';
                                             } else {
                                                 echo '<a href="./Login" class="btn btn-md bg-dark cart-button text-white w-100">Login</a>';
                                             }
