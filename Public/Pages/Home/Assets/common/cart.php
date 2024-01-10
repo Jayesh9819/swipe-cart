24e19aea264b4049b1b1505eeda51c56516de3c6
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-10 15:19:35 +0530

    Update cart.php

--- Public/Pages/Home/Assets/common/cart.php
+++ Public/Pages/Home/Assets/common/cart.php
@@ -15,60 +15,52 @@
             if ($result) {
                 // Fetch all rows (if there are multiple products in the cart)
                 $cart = $result->fetch_all(MYSQLI_ASSOC);
-                //print_r($cart);
+
+                // Iterate over each product in the cart
+                foreach ($cart as $product) {
+                    echo '<li class="product-box-contain">
+                            <div class="drop-cart">
+                                <a href="product-left-thumbnail.html" class="drop-image">
+                                    <img src="' . $product['main_img'] . '" class="blur-up lazyload" alt="">
+                                </a>
+                                <div class="drop-contain">
+                                    <a href="./productpage?id=' . $product['id'] . '">
+                                        <h5>' . $product['product_name'] . '</h5>
+                                    </a>
+                                    <h6><span>1 x</span>' . $product['coust_price'] . '</h6>
+                                    <button class="close-button close_button">
+                                        <i class="fa-solid fa-xmark"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </li>';
+                }
             } else {
                 // Handle the case where the query fails
                 echo "Error: " . $conn->error;
             }
-
-
-
-            echo '<li class="product-box-contain">
-            <div class="drop-cart">
-                <a href="product-left-thumbnail.html" class="drop-image">
-                    <img src="'.$cart['main_img'].'" class="blur-up lazyload"
-                        alt="">
-                </a>
-
-                <div class="drop-contain">
-                    <a href="./productpage?id='.$cart['id'].'">
-                        <h5>'.$cart['product_name'].'</h5>
-                    </a>
-                    <h6><span>1 x</span>'.$cart['coust_price'].'</h6>
-                    <button class="close-button close_button">
-                        <i class="fa-solid fa-xmark"></i>
-                    </button>
-                </div>
-            </div>
-        </li>
-        </ul>
-
-        <div class="price-box">
-            <h5>Total :</h5>
-            <h4 class="theme-color fw-bold">$106.58</h4>
-        </div>
-    
-        <div class="button-group">
-            <a href="./Cart" class="btn btn-sm cart-button">View Cart</a>
-            <a href="./Checkout" class="btn btn-sm cart-button theme-bg-color
-                                                        text-white">Checkout</a>
-        </div>
-    </div>';
-        } else {
-            echo                '    <ul class="cart-list">
- 
-            <div class="drop-contain">
-            <a href="./Login">
-                <h5>Login First</h5>
-            </a>
-        </div>
-        </ul>
-
-        ';
-        }
-
         ?>
+    </ul>
 
+    <div class="price-box">
+        <h5>Total :</h5>
+        <h4 class="theme-color fw-bold">$106.58</h4>
+    </div>
 
-
+    <div class="button-group">
+        <a href="./Cart" class="btn btn-sm cart-button">View Cart</a>
+        <a href="./Checkout" class="btn btn-sm cart-button theme-bg-color text-white">Checkout</a>
+    </div>
 </div>
+
+<?php
+        } else {
+            echo '<ul class="cart-list">
+            <div class="drop-contain">
+                <a href="./Login">
+                    <h5>Login First</h5>
+                </a>
+            </div>
+          </ul>';
+        }
+?>
